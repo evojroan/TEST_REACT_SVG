5df319a5f7ab5dd2a753c7e239057f00edf8c62b
evojroan <roan6903@gmail.com>
2023-02-03 22:47:28 +0800

    TEST_REACT_SVG

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+// import Cart from "./icons/cart.svg";
+// import PGComplete from "./icons/pg-complete.svg";
+import {ReactComponent as Cart} from "./icons/cart.svg"
+import {ReactComponent as Facebook} from "./icons/facebook.svg"
+import {ReactComponent as Instagram} from "./icons/instagram.svg"
+import {ReactComponent as LeftArrow} from "./icons/left-arrow.svg"
+import {ReactComponent as Minus} from "./icons/minus.svg"
+import {ReactComponent as Moon} from "./icons/moon.svg"
+import {ReactComponent as PGComplete} from "./icons/pg-complete.svg"
+import {ReactComponent as Plus} from "./icons/plus.svg"
+import {ReactComponent as RightArrow} from "./icons/right-arrow.svg"
+import {ReactComponent as Search} from "./icons/search.svg"
+import {ReactComponent as Sun} from "./icons/sun.svg"
+import {ReactComponent as Toggle} from "./icons/toggle.svg"
+import {ReactComponent as Whatsapp} from "./icons/whatsapp.svg"
+
+
+function App() {
+  return (
+    <>
+    <div><Cart/></div>
+    <div><Facebook/></div>
+    <div><Instagram/></div>
+    <div><LeftArrow/></div>
+    <div><Minus/></div>
+    <div><Moon/></div>
+    <div><PGComplete/></div>
+    <div><Plus/></div>
+    <div><RightArrow/></div>
+    <div><Search/></div>
+    <div><Sun/></div>
+    <div><Toggle/></div>
+    <div><Whatsapp/></div>
+    </>
+      );
+}
+
+export default App;
